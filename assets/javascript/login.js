73ad872e5314c8b56f1ea481a9ca1bdf380ac69a
mytoehertz <juanitosemail@gmail.com>
2019-02-28 21:37:31 -0500

    added firebase links, need to configure listeners. Authentication has been added already.

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -21,3 +21,19 @@ $(".sing-up").click(function(e) {
 	}
 
 });
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCJPmR2V6_XlC7y5dKsKlDZQU2PRICNBrk",
+    authDomain: "project-awesome-jaeje.firebaseapp.com",
+    databaseURL: "https://project-awesome-jaeje.firebaseio.com",
+    projectId: "project-awesome-jaeje",
+    storageBucket: "project-awesome-jaeje.appspot.com",
+    messagingSenderId: "741517517090"
+  };
+  firebase.initializeApp(config);
+
+// function startQuestion() {
+//     $(".question").text(question1.question);
+
+// }
