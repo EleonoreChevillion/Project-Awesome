a1272d42e0aac044050554cd1d15288e8783ef27
mytoehertz <juanitosemail@gmail.com>
2019-03-02 10:03:32 -0500

    Quick update

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -33,7 +33,20 @@ var config = {
   };
   firebase.initializeApp(config);
 
-// function startQuestion() {
-//     $(".question").text(question1.question);
-
-// }
+  firebase.auth().onAuthStateChanged(function(user) {
+      console.log("USER STATUS CHANGED " + user);
+    if (user) {
+      // User is signed in.
+    //   var displayName = user.displayName;
+    //   var email = user.email;
+    //   var emailVerified = user.emailVerified;
+    //   var photoURL = user.photoURL;
+    //   var isAnonymous = user.isAnonymous;
+    //   var uid = user.uid;
+    //   var providerData = user.providerData;
+      // ...
+    } else {
+      // User is signed out.
+      // ...
+    }
+  });
