4478a9ca64eeffe66de082b5b932b09467a81d55
mytoehertz <juanitosemail@gmail.com>
2019-03-05 16:16:20 -0500

    added firebase log in, email verification and registration function

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -22,6 +22,80 @@ $(".sing-up").click(function(e) {
 
 });
 
+//log in function
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    document.getElementById("user-div").style.display = "block";
+    document.getElementById("login-div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+    if(user != null){
+
+      let email_id = user.email;
+      let email_verified = user.emailVerified;
+
+      document.getElementById("user-paragraph").innerHTML = "Welcome User : " +email_id;
+    }
+  } else {
+    // No user is signed in.
+    document.getElementById("user-div").style.display = "none";
+    document.getElementById("login-div").style.display = "block";
+  }
+});
+
+function login(){
+  // window.alert(userEmail + "You have logged in" + userPassword);
+  let userEmail = document.getElementById("name").value;
+  let userPassword = document.getElementById("password").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+    // ...
+  });
+Redirect();
+}
+//create an account aka register
+function create_account(){
+  let userEmail = document.getElementById("name").value;
+  let userPassword = document.getElementById("password").value;
+
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+    // ...
+  });
+Redirect();
+
+}
+//email verification
+
+function send_verification(){
+
+  var user = firebase.auth().currentUser;
+
+  user.sendEmailVerification().then(function() {
+    // Email sent.
+  }).catch(function(error) {
+    // An error happened.
+  });
+
+}
+
+//logout function
+
+function logout(){
+  firebase.auth().signOut()
+}
+
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyCJPmR2V6_XlC7y5dKsKlDZQU2PRICNBrk",
@@ -33,23 +107,23 @@ var config = {
   };
   firebase.initializeApp(config);
 
-  firebase.auth().onAuthStateChanged(function(user) {
-      console.log("USER STATUS CHANGED " + user);
-    if (user) {
-      // User is signed in.
-    //   var displayName = user.displayName;
-    //   var email = user.email;
-    //   var emailVerified = user.emailVerified;
-    //   var photoURL = user.photoURL;
-    //   var isAnonymous = user.isAnonymous;
-    //   var uid = user.uid;
-    //   var providerData = user.providerData;
-      // ...
-    } else {
-      // User is signed out.
-      // ...
-    }
-  });
+  // firebase.auth().onAuthStateChanged(function(user) {
+  //     console.log("USER STATUS CHANGED " + user);
+  //   if (user) {
+  //     // User is signed in.
+  //   //   var displayName = user.displayName;
+  //   //   var email = user.email;
+  //   //   var emailVerified = user.emailVerified;
+  //   //   var photoURL = user.photoURL;
+  //   //   var isAnonymous = user.isAnonymous;
+  //   //   var uid = user.uid;
+  //   //   var providerData = user.providerData;
+  //     // ...
+  //   } else {
+  //     // User is signed out.
+  //     // ...
+  //   }
+  // });
 
   function Redirect() {
     window.location.href = "questionnaire.html";
